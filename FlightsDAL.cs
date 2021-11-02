b919ddb4bc6896123dce1e271975a0f87b773d37
karthik-saripalli <70575291+karthik-saripalli@users.noreply.github.com>
2021-11-02 17:48:14 +0530

    Add files via upload

---
+++ FlightsDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using BALFlights;
+
+namespace DALFlights
+{
+    public class FlightsDAL
+    {
+        public DataTable showAllDetails()
+        {
+            SqlConnection con = new SqlConnection("server=LAPTOP-362TBH8P\\SQLEXPRESS;integreated security=true;database=FlightDB");
+            SqlDataAdapter da = new SqlDataAdapter("select * from Flights", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Flights");
+            return ds.Tables[0];
+        }
+    }
+}
