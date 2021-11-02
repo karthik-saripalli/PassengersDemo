2ddd81aeebb25bea38bbfd188d17cc597d426d41
karthik-saripalli <70575291+karthik-saripalli@users.noreply.github.com>
2021-11-02 17:51:06 +0530

    Add files via upload

---
+++ FlightsBAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALFlights
+{
+    public class FlightsBAL
+    {
+        public int FlightId { get; set; }
+        public string FlightName { get; set; }
+        public DateTime FlightArrival { get; set; }
+        public DateTime FlightDeparture { get; set; }
+        public int PassengersCount { get; set; }
+        public int CaptainID { get; set; }
+    }
+}
